a4fb652a18045b95745d3cfab174ca29458b0f4a
JulioJohan <75770163+JulioJohan@users.noreply.github.com>
2022-10-12 10:39:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const corsOptions = {
         }
     }
 }
-app.use(cors(corsOptions));
+app.use(cors(whitelist));
 //req son los datos que tu envias
 //res es la respuesta que estas obteniendo por esa peticion
 
